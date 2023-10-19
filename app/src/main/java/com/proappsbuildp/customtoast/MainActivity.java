59e6a0568babbfa37fcea8d87e93fa9c75362b58
mdsiamulislam <mdsiamulislams@gmail.com>
2023-10-19 08:12:55 +0600

    Add Audio

--- app/src/main/java/com/proappsbuildp/customtoast/MainActivity.java
+++ app/src/main/java/com/proappsbuildp/customtoast/MainActivity.java
@@ -1,42 +1,39 @@
 package com.proappsbuildp.customtoast;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.content.Intent;
-import android.net.Uri;
+import android.media.MediaPlayer;
 import android.os.Bundle;
-import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 public class MainActivity extends AppCompatActivity {
 
     ImageView imageView;
     TextView textView;
-
+    MediaPlayer mediaPlayer;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        mediaPlayer = MediaPlayer.create(this, R.raw.toast_sound);
+
         imageView = findViewById(R.id.imageView);
         textView = findViewById(R.id.textView);
-        imageView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
+        imageView.setOnClickListener(v -> {
+            mediaPlayer.start();
+            LayoutInflater layoutInflater = getLayoutInflater();
+            View view = layoutInflater.inflate(R.layout.custom_toast, findViewById(R.id.customtoast));
 
-                LayoutInflater layoutInflater = getLayoutInflater();
-                View view = layoutInflater.inflate(R.layout.custom_toast, findViewById(R.id.customtoast));
+            Toast toast = new Toast(MainActivity.this);
+            toast.setDuration(Toast.LENGTH_LONG);
+            toast.setView(view);
+            toast.show();
 
-                Toast toast = new Toast(MainActivity.this);
-                toast.setDuration(Toast.LENGTH_LONG);
-                toast.setView(view);
-                toast.show();
-
-            }
         });
     }
 }
